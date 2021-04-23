4b221e79a8d124c7cac8f91a0f9595f0381ab9db
ibanjo12 <j.nanjari.m@gmail.com>
2021-04-23 04:10:48 -0400

    fix conection db

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require ("express");
-const mongoose = require ("mongoose");
-const morgan  = require ("morgan");
-const bodyParser  = require ("body-parser");
-const cookieParser  = require ("cookie-parser");
-const expressValidator  = require ("express-validator");
-const cors =  require("cors");
-require ("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
+const cors = require("cors");
+require("dotenv").config();
 
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/users");
@@ -17,15 +17,31 @@ const paymentRoutes = require("./routes/payment");
 // app
 const app = express();
 
-// db
 mongoose
-    .connect(process.env.DATABASE, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify:false,
-        uri: { useFindAndModify: false }
-    })
-    .then(() => console.log("DB Connected"));
+.connect(process.env.DATABASE, {
+useUnifiedTopology: true,
+useNewUrlParser: true,
+useCreateIndex: true,
+})
+.then(() => console.log('DB Connected!'))
+.catch(err => {
+console.log('db not connected');
+});
+
+
+
+
+
+
+// db
+/* mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    //uri: { useFindAndModify: false },
+  })
+  .then(() => console.log("DB Connected")); */
 
 // middlewares
 app.use(morgan("dev"));
@@ -35,19 +51,16 @@ app.use(expressValidator());
 app.use(cors());
 // routes middleware
 
-app.use ("/api", authRoutes);
-app.use ("/api", userRoutes);
-app.use ("/api", roomsRoutes);
-app.use ("/api", bookingRoutes);
-app.use ("/api", paymentRoutes);
-app.use ("/api", passengerRoutes);
-
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", roomsRoutes);
+app.use("/api", bookingRoutes);
+app.use("/api", paymentRoutes);
+app.use("/api", passengerRoutes);
 
 //db
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
-
-
